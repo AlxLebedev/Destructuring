b60ccec1a0db718a1b502bbcd4f061b8ed580792
Alexey Lebedev <vorresti@ya.ru>
2020-02-26 15:24:16 +0300

    forEach instead "for"

--- src/js/showSpecialAttack.js
+++ src/js/showSpecialAttack.js
@@ -2,13 +2,14 @@
 export default function showSpecialAttack(obj) {
   const { special } = obj;
   const arrSpecialAttack = [];
-  for (let i = 0; i < special.length; i += 1) {
+  special.forEach((element) => {
     const {
       id, name, icon, description = 'Описание недоступно',
-    } = special[i];
+    } = element;
     arrSpecialAttack.push({
       id, name, icon, description,
     });
-  }
+  });
+
   return arrSpecialAttack;
 }
